dd01e3d8356a4b6110a0b528edbcd0d7aff645e3
Julian Hecker <julianheckerdev@gmail.com>
2025-04-22 23:01:53 -0400

    make auth edge compatible

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import NextAuth from 'next-auth';
-import type { JWT as _ } from 'next-auth/jwt';
+import type { JWT } from 'next-auth/jwt';
 import type { Provider } from 'next-auth/providers';
 import Google from 'next-auth/providers/google';
-
-import { createStripeCustomer, getCustomerByEmail } from '@/services/customer';
+import Stripe from 'stripe';
 
 // https://authjs.dev/getting-started/typescript?framework=next-js#module-augmentation
 declare module 'next-auth' {
@@ -32,6 +31,7 @@ declare module 'next-auth' {
 declare module 'next-auth/jwt' {
     /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
     interface JWT {
+        email: string;
         stripeCustomerId: string;
         isAdmin: boolean;
     }
@@ -48,25 +48,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             const { token, trigger } = params;
             if (!trigger || !token?.email) return token;
 
-            console.log('==== JWT ====');
-            console.log(JSON.stringify(params));
-
             // Find customer associated with user's email address
-            let customer = await getCustomerByEmail(token.email);
-            // ...Or create a new customer
-            if (!customer) {
-                customer = await createStripeCustomer({
-                    email: token.email,
-                    name: token.name ?? 'Customer',
-                    metadata: { image: token.picture ?? null },
-                });
-            }
+            let customer = await getStripeCustomerByEmailWithFetch(token.email);
 
-            // todo: update stripe profile each time, as this part of the callback hits on each login
-            // await stripe.customers.update(customer.id, { metadata: {} });
+            // update stripe profile each time, as this part of the callback hits on each login
+            if (customer) await updateStripeCustomerWithFetch(customer.id, token);
+
+            // ...Or create a new customer
+            if (!customer) customer = await createStripeCustomerWithFetch(token);
 
             token.stripeCustomerId = customer.id;
-            token.isAdmin = !!customer.metadata.isAdmin;
+            token.isAdmin = !!customer.metadata?.isAdmin;
 
             return token;
         },
@@ -82,3 +74,68 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 });
 
 export const getUser = async () => (await auth())?.user;
+
+// Using fetch instead of stripe SDK for edge function support
+const STRIPE_CUSTOMER_API_URL = 'https://api.stripe.com/v1/customers';
+
+async function getStripeCustomerByEmailWithFetch(email: string): Promise<Stripe.Customer | null> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + process.env.STRIPE_SECRET_KEY);
+    const response = await fetch(`${STRIPE_CUSTOMER_API_URL}?email=${email}`, { headers });
+    const customer = (await response.json())?.data[0] ?? null;
+    return customer;
+}
+
+async function createStripeCustomerWithFetch(token: JWT): Promise<Stripe.Customer> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + process.env.STRIPE_SECRET_KEY);
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    const body = new URLSearchParams({
+        email: token.email,
+        name: token.name ?? 'Customer',
+        'metadata[image]': token.picture ?? '',
+    });
+
+    const response = await fetch(STRIPE_CUSTOMER_API_URL, {
+        method: 'POST',
+        headers,
+        body,
+    });
+
+    if (!response.ok) {
+        const message = await response.text();
+        throw new Error(`Failed to create Stripe Customer: ${message}`);
+    }
+
+    const customer = await response.json();
+    return customer;
+}
+
+async function updateStripeCustomerWithFetch(
+    customerId: string,
+    token: JWT
+): Promise<Stripe.Customer> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + process.env.STRIPE_SECRET_KEY);
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    const body = new URLSearchParams({
+        name: token.name ?? '',
+        'metadata[image]': token.picture ?? '',
+    });
+
+    const response = await fetch(`${STRIPE_CUSTOMER_API_URL}/${customerId}`, {
+        method: 'POST',
+        headers,
+        body,
+    });
+
+    if (!response.ok) {
+        const message = await response.text();
+        throw new Error(`Failed to update Stripe Customer: ${message}`);
+    }
+
+    const customer = await response.json();
+    return customer;
+}
