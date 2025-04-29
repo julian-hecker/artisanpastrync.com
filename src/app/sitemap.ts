e4a4a1bbe2eba3c20e43cf5ae8f74fedefbac87a
Julian Hecker <julianheckerdev@gmail.com>
2025-04-29 14:45:40 -0400

    Revert "Merge pull request #1 from julian-hecker/fix/isr-sitemap"
    This reverts commit f22b4e7bcdeb97394949b979ecb0be4477705407, reversing
    changes made to e7d4a05d3d4dfb096f760a2433d499a4fb163b21.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,13 @@
 import { MetadataRoute } from 'next';
-import { unstable_cache } from 'next/cache';
 
-import { TAGS } from '@/constants/cache-tags';
 import { SITE_URL } from '@/constants/environment';
 import { nameToSlug } from '@/lib/utils';
 import { getAllProducts } from '@/services/product';
 
-const getProductsCached = unstable_cache(() => getAllProducts(), [], { tags: [TAGS.products] });
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const paths = ['', '/about', '/products', '/login'];
 
-    const products = await getProductsCached();
+    const products = await getAllProducts();
     const productEntries = products.map((product) => ({
         url: `${SITE_URL}/products/${nameToSlug(product.name)}`,
         lastModified: new Date(product.updated * 1000),
