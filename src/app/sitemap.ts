40d19314998a94c2de938866d05186455a97ad2d
Julian Hecker <julianheckerdev@gmail.com>
2025-04-29 14:33:12 -0400

    possibly make sitemaps update using isr

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,17 @@
 import { MetadataRoute } from 'next';
+import { unstable_cache } from 'next/cache';
 
+import { TAGS } from '@/constants/cache-tags';
 import { SITE_URL } from '@/constants/environment';
 import { nameToSlug } from '@/lib/utils';
 import { getAllProducts } from '@/services/product';
 
+const getProductsCached = unstable_cache(() => getAllProducts(), [], { tags: [TAGS.products] });
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const paths = ['', '/about', '/products', '/login'];
 
-    const products = await getAllProducts();
+    const products = await getProductsCached();
     const productEntries = products.map((product) => ({
         url: `${SITE_URL}/products/${nameToSlug(product.name)}`,
         lastModified: new Date(product.updated * 1000),
